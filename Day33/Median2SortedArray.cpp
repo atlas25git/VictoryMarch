b71781025b55f2f3ff227309cbb3198ccb9fb54b
atlas25git <atlas.sarthak@gmail.com>
2021-06-23 23:51:18 +0530

    revision

--- Day33/Median2SortedArray.cpp
+++ Day33/Median2SortedArray.cpp
@@ -20,7 +20,7 @@ class Solution
     		//Searching on right 
             if(i < n && j > 0 && brr[j-1] >= arr[i]){
                 min_i = i+1;
-            }
+            }//
             //--and thus it is done
             //if i = 0, it means that elements from arr[] in the first half is 
     		//an empty set and if j = m, it means that elements from brr[] in 
