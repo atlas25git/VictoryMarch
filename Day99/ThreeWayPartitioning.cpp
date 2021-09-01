3c3e73095329ed72bd57829ee0eb7477130726a3
atlas25git <atlas.sarthak@gmail.com>
2021-09-01 19:37:10 +0530

    SortingDone

---
+++ Day99/ThreeWayPartitioning.cpp
@@ -0,0 +1,19 @@
+class Solution{   
+public:
+    //Function to partition the array around the range such 
+    //that array is divided into three parts.
+    void threeWayPartition(vector<int>& ar,int a, int b)
+    {
+        int l=0,r=ar.size()-1;
+        int itr = 0;
+        while(itr<=r)
+        {
+            if(ar[itr] < a)//element for first window
+                swap(ar[itr++],ar[l++]);
+            else if(ar[itr]>b)
+                //itr not incremented as we want to check the element for 'a'
+                swap(ar[itr],ar[r--]);
+            else itr++;
+        }
+    }
+};
