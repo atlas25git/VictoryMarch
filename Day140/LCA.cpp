ea0a57676869a68f81bd0c3bc3cf590b3e19bf53
atlas25git <atlas.sarthak@gmail.com>
2021-10-13 14:57:56 +0530

    ind

---
+++ Day140/LCA.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to return the lowest common ancestor in a Binary Tree.
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       if(!root)return root;
+       Node* l,*r;
+
+       if(root->data == n1 || root->data ==n2)return root;
+        //since l and r are called on the same root ,hence will 
+        //contain a common node.
+       l = lca(root->left,n1,n2);
+       r = lca(root->right,n1,n2);
+       
+       if(l&&r)return root;
+       
+       else return l?l:r;
+    }
+};
