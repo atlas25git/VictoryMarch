52d895bb5573e3b8df40decd423e53d6e77f2b6c
atlas25git <atlas.sarthak@gmail.com>
2021-11-10 12:45:18 +0530

    bstComplete

---
+++ Day168/floorBST.cpp
@@ -0,0 +1,32 @@
+int floor(Node* root, int key) 
+{ 
+	if(!root)return -1;
+	Node* res = NULL;
+	
+	while(root)
+	{
+	    if(root->data == key)
+	    {
+	        return root->data;
+	    }
+	    else if(root->data > key)
+	        
+	        {
+	        //if the current root's data is gr8r than key
+	        //essentially our data lies in left of ST
+	        root = root->left;
+	        }
+	    else if(root->data < key)
+	    {
+	       //here the contingency lies that here could be some
+	       //number gr8r than current data but smlr than key
+	       //thus the more legitimate floor
+	       //in res thus we maintian the order of these events
+	       res= root;
+	       root=root->right;
+	    }
+	    
+	}
+	//cout<<res->data;
+	return !res?-1:res->data;
+}
---
+++ Day168/topView.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        map<int,int> mp;
+        queue<pair<Node*,int>> q;
+        q.push({root,0});
+        while(!q.empty())
+        {
+            auto t = q.front();
+            q.pop();
+            if(mp.count(t.second) == 0)
+                mp[t.second] = t.first->data;
+            
+            if(t.first->left)
+                q.push({t.first->left,t.second-1});
+            if(t.first->right)
+                q.push({t.first->right,t.second+1});
+        }
+        vector<int> ans;
+        for(auto x: mp)
+            ans.push_back(x.second);
+        return ans;
+    }
+};
