cd41df056bdc010d1865b16f8ecc82539d374502
atlas25git <atlas.sarthak@gmail.com>
2021-11-11 21:29:45 +0530

    heaps

--- Day169/medianOfStream.cpp
+++ Day169/medianOfStream.cpp
@@ -1,22 +1,28 @@
 class Solution
-{   double median = 0;
+{   
+    private:
     priority_queue<int> max;
     priority_queue<int,vector<int>,greater<int>> min;
+    double median = 0;
+    
     public:
     //Function to insert heap.
     void insertHeap(int &x)
     {
-        if(max.empty())
-            max.push(x);
-        else if(x > max.top())
-            min.push(x);
-        else if(min.empty()) //&& x<max.top())//second clause redundant
-            min.push(max.top()),
-            max.pop(),
-            max.push(x);
-        else
-            max.push(x);
-        
+        if(max.empty())max.push(x);
+        else if(x>max.top())min.push(x);
+        else if(x<=max.top())
+        {
+            //if it's second insertion and min is empty
+            //since at any given point the abs diff b/2 both
+            //the heaps would be <= 1
+            if(min.empty())
+                min.push(max.top()),
+                max.pop(),
+                max.push(x);
+            else
+                max.push(x);
+        }
         balanceHeaps();
     }
     
@@ -26,14 +32,11 @@ class Solution
         if(abs(max.size() - min.size()) > 1)
         {
             if(max.size() > min.size())
-            {
-                min.push(max.top());
+                min.push(max.top()),
                 max.pop();
-            }
-            else{
-                max.push(min.top());
+            else if(min.size() > max.size())
+                max.push(min.top()),
                 min.pop();
-            }
         }
     }
     
@@ -41,19 +44,12 @@ class Solution
     double getMedian()
     {
         if((max.size() + min.size())%2 == 0)
-        {
-            median = max.top() + min.top();
-            median /= 2;
-        }
-        else{
-            if(min.empty())
-                median = max.top();
-            else if(min.size() < max.size())
-                median = max.top();
-            else 
+            median = (max.top() + min.top())/2.0;
+        else {
+            if(min.size() > max.size())
                 median = min.top();
+            else median = max.top();
         }
-        
         return median;
     }
 };
